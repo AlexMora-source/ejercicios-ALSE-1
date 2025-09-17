45b49aa6738dd18fcea71f0d6d8a2e5242483271
AlexMora-source <alexandermora552@gmail.com>
2025-09-16 21:13:16 -0500

    Create ejercicio_MCD.cpp

---
+++ ejercicio_MCD.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n1, n2;
+  cout << "Ingrese los valores de los numeros de tipo entero: ";
+  cout << "\n";
+  cout << "Primer nunmero: ";
+  cin >> n1;
+  cout << "Segundo numero: ";
+  cout << "\n";
+  bool MCD = false;
+  if n1/2 and n2/2;
